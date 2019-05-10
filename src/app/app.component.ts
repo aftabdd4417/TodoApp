cc8302094c5da9bdb60a4480d0901ea3985f6233
aftabdd4417 <aftab@excellencetechnologies.info>
2019-05-10 12:09:23 +0530

    task1 completed

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,32 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"]
 })
 export class AppComponent {
-  title = 'TodoApp';
+  title = "Todo List Items";
+  todoArray = [
+    "Apple",
+    "Mango",
+    "Banana",
+    "Papaya",
+    "Graps",
+    "Govava",
+    "Lichi"
+  ];
+  favouriteFruite: any = this.todoArray[1];
+  task: any = "";
+  alertmsg: boolean = false;
+
+  onClick() {
+    if (this.task == "") {
+      this.alertmsg = true;
+    } else {
+      this.alertmsg = false;
+      this.todoArray.push(this.task);
+      this.task = "";
+    }
+  }
 }
